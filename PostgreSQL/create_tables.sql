89607119b3c8843dee5eecbdae0266ed327058e8
Costin Busioc <busioccostin@gmail.com>
2020-04-11 13:40:09 +0000

    Add database creation script

---
+++ PostgreSQL/create_tables.sql
@@ -0,0 +1,26 @@
+CREATE TABLE "students" (
+  "id" SERIAL PRIMARY KEY,
+  "full_name" varchar,
+  "lives_in_campus" boolean,
+  "origin_city" varchar
+);
+
+CREATE TABLE "professors" (
+  "id" SERIAL PRIMARY KEY,
+  "full_name" varchar,
+  "specialization" varchar,
+  "office_room" varchar
+);
+
+CREATE TABLE "thesis" (
+  "id" SERIAL PRIMARY KEY,
+  "student_id" int,
+  "professor_id" int,
+  "thesis_area" varchar,
+  "thesis_title" varchar
+);
+
+ALTER TABLE "thesis" ADD FOREIGN KEY ("student_id") REFERENCES "students" ("id");
+
+ALTER TABLE "thesis" ADD FOREIGN KEY ("professor_id") REFERENCES "professors" ("id");
+
